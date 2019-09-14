d0bfd8971c49796ae58e59b795eab98a842f926e
desz <dg8williams@gmail.com>
2019-09-14 12:47:44 +0100

    ..

---
+++ src/test/java/com/lendico/loan/annuity/rest/AnnuityRestControllerTest.java
@@ -0,0 +1,62 @@
+package com.lendico.loan.annuity.rest;
+
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.lendico.loan.annuity.scheduler.PaymentScheduler;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class AnnuityRestControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private PaymentScheduler annuityScheduler;
+
+	/*
+	 * { "nominalRate": "5.0", "duration": 24, "startDate": "2018-01-01T00:00:01Z" }
+	 */
+	@Test
+	public void createSchedule_NullLoanAmt_400() throws Exception {
+
+		String annuityRequest = "{\"nominalRate\": 5.0, \"duration\": 24, \"startDate\": \"2018-01-01T00:00:01Z\"}";
+
+		mockMvc.perform(
+				post("/generate-plan").content(annuityRequest).header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.timestamp", is(notNullValue()))).andExpect(jsonPath("$.status", is(400)))
+				.andExpect(jsonPath("$.errors").isArray()).andExpect(jsonPath("$.errors", hasSize(1)))
+				.andExpect(jsonPath("$.errors", hasItem("must be greater than or equal to 1000.00")));
+
+		verify(annuityScheduler, times(0)).createScheduler(anyString(), anyInt(), anyDouble(), anyDouble());
+
+	}
+
+}
